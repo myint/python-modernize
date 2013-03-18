661c6794cb3ec0fa825f46d80e5fd510e0cd7ee3
Steven Myint <git@stevenmyint.com>
2013-03-17 19:34:03 -0700

    Limit to 2.7 and 3.3

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.3'
     ]
 )
