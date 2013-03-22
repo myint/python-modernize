f2e245dd2e36260083cb0302ccfaca7abe84e4e9
Steven Myint <git@stevenmyint.com>
2013-03-22 14:59:41 -0700

    Format

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@
 import os
 from setuptools import setup
 
-readme = open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r').read()
-
-setup(
-    name='modernize',
-    author='Armin Ronacher',
-    author_email='armin.ronacher@active-4.com',
-    version='0.2.1',
-    url='http://github.com/mitsuhiko/python-modernize',
-    packages=['libmodernize', 'libmodernize.fixes'],
-    description='A hack on top of 2to3 for modernizing code for '
-                'hybrid codebases.',
-    long_description=readme,
-    entry_points={
-        'console_scripts': [
-            'python-modernize = libmodernize.main:main'
+with open(os.path.join(os.path.dirname(__file__),
+                       'README.rst'), 'r') as readme:
+    setup(
+        name='modernize',
+        author='Armin Ronacher',
+        author_email='armin.ronacher@active-4.com',
+        version='0.2.1',
+        url='http://github.com/mitsuhiko/python-modernize',
+        packages=['libmodernize', 'libmodernize.fixes'],
+        description='A hack on top of 2to3 for modernizing code for '
+                    'hybrid codebases.',
+        long_description=readme.read(),
+        entry_points={
+            'console_scripts': [
+                'python-modernize = libmodernize.main:main'
+            ]
+        },
+        zip_safe=False,
+        classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7'
+            'Programming Language :: Python :: 3.3'
         ]
-    },
-    zip_safe=False,
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3.3'
-    ]
-)
+    )
