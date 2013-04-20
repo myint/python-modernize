13901747322017178cd5808044c7752eeccda059
Steven Myint <git@stevenmyint.com>
2013-04-20 10:28:02 -0700

    Enable Python 2.6 tests

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ with open(os.path.join(os.path.dirname(__file__),
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7'
+            'Programming Language :: Python :: 2.6'
+            'Programming Language :: Python :: 3.2'
             'Programming Language :: Python :: 3.3'
         ]
     )
