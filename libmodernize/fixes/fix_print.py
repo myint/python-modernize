a3ccfbb0eaa8d3aaca0b5f5020ef67218bdd920d
Steven Myint <git@stevenmyint.com>
2013-04-20 08:27:30 -0700

    Do not add future print import unless necessary

--- libmodernize/fixes/fix_print.py
+++ libmodernize/fixes/fix_print.py
@@ -13,10 +13,11 @@ No changes are applied if print_function is imported from __future__
 
 """
 
-# Local imports
 from lib2to3 import patcomp, pytree, fixer_base
 from lib2to3.pgen2 import token
-from lib2to3.fixer_util import Name, Call, Comma, String
+from lib2to3.fixer_util import Name, Call, Comma, FromImport, Newline, String
+
+from libmodernize import check_future_import
 
 parend_expr = patcomp.compile_pattern(
     """atom< '(' [atom|STRING|NAME] ')' >"""
@@ -31,7 +32,18 @@ class FixPrint(fixer_base.BaseFix):
               simple_stmt< any* bare='print' any* > | print_stmt
               """
 
+    def start_tree(self, tree, filename):
+        self.found_print = False
+
+    def new_future_import(self, old):
+        new = FromImport('__future__',
+                         [Name('print_function', prefix=' ')])
+        if old is not None:
+            new.prefix = old.prefix
+        return new
+
     def transform(self, node, results):
+        self.found_print = True
         assert results
 
         bare_print = results.get('bare')
@@ -82,3 +94,47 @@ class FixPrint(fixer_base.BaseFix):
             l_nodes.append(Comma())
             n_argument.prefix = ' '
         l_nodes.append(n_argument)
+
+    def finish_tree(self, tree, filename):
+        """Add future print import.
+
+        From:
+            https://github.com/facebook/tornado
+
+        """
+        if not self.found_print:
+            return
+
+        for node in tree.children:
+            if 'print_function' in check_future_import(node):
+                # already imported
+                return
+
+        if not isinstance(tree, pytree.Node):
+            # Empty files (usually __init__.py) show up as a single Leaf
+            # instead of a Node, so leave them alone
+            return
+        first_stmt = tree.children[0]
+        if is_docstring(first_stmt):
+            # Skip a line and add the import after the docstring
+            tree.insert_child(1, Newline())
+            pos = 2
+        elif first_stmt.prefix:
+            # No docstring, but an initial comment (perhaps a #! line).
+            # Transfer the initial comment to a new blank line.
+            newline = Newline()
+            newline.prefix = first_stmt.prefix
+            first_stmt.prefix = ''
+            tree.insert_child(0, newline)
+            pos = 1
+        else:
+            # No comments or docstring, just insert at the start
+            pos = 0
+        tree.insert_child(pos, self.new_future_import(None))
+        tree.insert_child(pos + 1, Newline())  # terminates the import stmt
+
+
+# copied from fix_tuple_params.py
+def is_docstring(stmt):
+    return isinstance(stmt, pytree.Node) and \
+        stmt.children[0].type == token.STRING
