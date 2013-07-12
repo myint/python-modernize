4b332c246514ecd5b04cf7764ea338e4d236d272
Steven Myint <git@stevenmyint.com>
2013-07-11 19:51:21 -0700

    Clean up

--- libmodernize/fixes/fix_absolute_import_future.py
+++ libmodernize/fixes/fix_absolute_import_future.py
@@ -6,20 +6,13 @@ from lib2to3.pygram import python_symbols
 
 from libmodernize import add_future_import
 
+
 class FixAbsoluteImportFuture(fix_import.FixImport):
 
     def start_tree(self, tree, name):
         super(FixAbsoluteImportFuture, self).start_tree(tree, name)
-        self.found_import = False
-
-    def transform(self, node, results):
-        value = super(FixAbsoluteImportFuture, self).transform(node, results)
-        if value:
-            self.found_import = True
-        return value
 
     def finish_tree(self, tree, name):
-        #if self.found_import:
         for node in tree.children:
             if not (node.type == python_symbols.simple_stmt and node.children):
                 continue
