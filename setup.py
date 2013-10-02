a486232e55abc65b1546f4e18f3d2f6483eac76e
Steven Myint <git@stevenmyint.com>
2013-10-02 06:32:28 -0700

    Add missing quotes

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ with open('README.rst') as readme:
         classifiers=[
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7'
-            'Programming Language :: Python :: 2.6'
-            'Programming Language :: Python :: 3.2'
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3'
         ]
     )
