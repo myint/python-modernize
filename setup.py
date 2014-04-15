e31e3bc54708ec9f01aa9052bd1f37f43b515676
Steven Myint <git@stevenmyint.com>
2014-04-15 07:24:54 -0700

    Add missing classifier

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ with open('README.rst') as readme:
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3'
         ]
