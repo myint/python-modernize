2c2b7158bce6804f9b7bbe4d0e410fe7473acdd1
Steven Myint <git@stevenmyint.com>
2014-10-09 06:45:39 -0700

    Add missing comma in classifiers

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open('README.rst') as readme:
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3'
+            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
         ]
     )
